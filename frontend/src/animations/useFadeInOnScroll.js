fe75b3355789a340350971adfbfe10d070aae6d1
faizvk <faizvk14@gmail.com>
2026-01-31 18:09:30 +0530

    add lazy and suspence

--- frontend/src/animations/useFadeInOnScroll.js
+++ frontend/src/animations/useFadeInOnScroll.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export function useFadeInScroll() {
+export function useFadeInOnScroll() {
   const location = useLocation();
 
   useEffect(() => {
