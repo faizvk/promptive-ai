b4974bd2142e4d91b668f754fb6b9d9faa957b16
faizvk <faizvk14@gmail.com>
2026-05-10 00:19:35 +0530

    centralize env loading: validate required vars at boot, fail fast with missing list

--- backend/config/huggingface.js
+++ backend/config/huggingface.js
@@ -1,5 +1,6 @@
 import { InferenceClient } from "@huggingface/inference";
+import { HUGGINGFACE_API_KEY } from "./env.js";
 
-const hf = new InferenceClient(process.env.HUGGINGFACE_API_KEY);
+const hf = new InferenceClient(HUGGINGFACE_API_KEY);
 
 export default hf;
